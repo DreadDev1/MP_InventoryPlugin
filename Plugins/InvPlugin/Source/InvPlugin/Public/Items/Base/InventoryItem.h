1cb0b447629566ac3fb46c890c11c8daec941397
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-08-10 20:00:02 -0400

    Inventory Item

---
+++ Plugins/InvPlugin/Source/InvPlugin/Public/Items/Base/InventoryItem.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "InventoryItem.generated.h"
+
+USTRUCT(BlueprintType, Category = "Inventory")
+struct FInventoryItem 
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	FName UniqueName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory", meta = (ClampMin = 1.0))
+	int StackSize = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	int Index = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	bool bIsEmpty = false;
+
+	bool operator == (const FInventoryItem rhs) const { return UniqueName == rhs.UniqueName; }
+};
